2931174020c568bbaec7705a34169d3588aefe41
Koua Xiong <kxiong517@gmail.com>
2019-07-19 14:09:41 -0500

    added in appbar

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="main-header">
+    <Header/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './header.css';
+
+// Material UI
+import { AppBar } from '@material-ui/core';
+
+class Header extends Component {
+
+    render() {
+
+        return (
+            <>
+                <AppBar>Test</AppBar>
+            </>
+        )
+    }
+}
+
+export default Header;
